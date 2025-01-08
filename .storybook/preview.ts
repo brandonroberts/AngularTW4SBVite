89e3a223a4df56892dc3ce8f9eb07942cb5da43d
Brandon Roberts <robertsbt@gmail.com>
2025-01-08 16:53:14 -0600

    feat: show working Tailwind setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,6 @@
+import '../src/styles.scss'; // replace with the name of your tailwind css file
+import '@angular/material/prebuilt-themes/azure-blue.css';
+
 import type { Preview } from "@storybook/angular";
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
